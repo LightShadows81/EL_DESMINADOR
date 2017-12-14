3a90fbce9419bf273b09f3945c423e4b78de7a31
LightShadows81 <33803836+LightShadows81@users.noreply.github.com>
2017-12-14 08:18:48 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,86 +1,127 @@
-var totalBombes = 10;
-var grille = new Array();
-var l = 20;
-var colonne = 10;
-var ligne = 10;
+var totalRevelee=0;
+class Cell{
+  constructor(i,j,l) {
+    this.x = i*l;
+    this.y = j*l;
+    this.l = l;
+    this.i = i;
+    this.j = j;
+    this.revelee = false;
+    this.bombe = false;
+    this.bombe_voisine = 0;
+    this.drapeau = false;
+    this.win = false;
+  }
 
-function setup(){
-  createCanvas(401,401);
-  background(50,50,50);
-  noStroke();
-  initialise();
 
-  var options = [];
-  for (var i = 0; i < ligne; i++) {
-    for (var j = 0; j < colonne; j++) {
-      options.push([i, j]);
+  show(tot) {
+    if (totalRevelee==400-tot){
+      this.win = true;
+    }
+    stroke(0);
+    noFill();
+    rect(this.x, this.y, this.l, this.l);
+
+    if(this.win){
+      /*createCanvas(401,401);
+      background(0,255,0);
+      textAlign(CENTER);
+      var str = " YOU WIN ";
+      var result = str.fontcolor("red");
+      noLoop();
+      document.write(result);*/
+      alert("YOU WIN");
+
+    }
+    if(this.drapeau){
+      fill(0);
+      rect(this.x, this.y, this.l, this.l);
+    }
+    if(this.revelee){
+          if (this.bombe){
+            fill(120);
+            ellipse(this.x+this.l*0.5,this.y+this.l*0.5,this.l*0.5);
+            }else {
+
+              fill(200);
+              rect(this.x, this.y, this.l, this.l);
+
+              if (this.bombe_voisine > 0) {
+                textAlign(CENTER);
+                fill(0);
+                text(this.bombe_voisine, this.x + this.l * 0.5, this.y + this.l - 6);
+              }
+            }
+          }
+  }
+
+
+
+
+  contenant(x,y) {
+    return (x > this.x && x < this.x + this.l && y > this.y && y < this.y + this.l);
+  }
+
+  revele() {
+    this.revelee = true;
+    totalRevelee=totalRevelee+1;
+    if (this.bombe_voisine == 0) {
+      this.remplissage();
     }
   }
 
-  for (var n = 0; n < totalBombes; n++) {
-    var index = floor(random(options.length));
-    var choice = options[index];
-    var i = choice[0];
-    var j = choice[1];
-    // Deletes that spot so it's no longer an option
-    options.splice(index, 1);
-    grille[i][j].bombe = true;
-  }
-
-  for (var i = 0; i < ligne; i++) {
-    for (var j = 0; j < colonne; j++) {
-      grille[i][j].compterBombe();
+  compterBombe(){
+    if(this.bombe){
+      this.bombe_voisine = -1;
+      return;
     }
+    var total = 0;
+    for (var xoff = -1; xoff <= 1; xoff++) {
+      var i = this.i + xoff;
+      if (i < 0 || i >= ligne) continue;
+
+      for (var yoff = -1; yoff <= 1; yoff++) {
+        var j = this.j + yoff;
+        if (j < 0 || j >= colonne) continue;
+
+        var voisine = grille[i][j];
+        if (voisine.bombe) {
+          total++;
+        }
+      }
+    }
+    this.bombe_voisine = total;
   }
 
-}
+  remplissage(xoff,yoff) {
+    for (var xoff = -1; xoff <= 1; xoff++) {
+      var i = this.i + xoff;
+      if (i < 0 || i >= ligne) continue;
 
-function initialise(){
-  for(var i=0; i<ligne; i++){
-		grille[i] = new Array();
-	}
-  for(var i=0; i<ligne; i++){
-		for(var j=0; j<colonne; j++){
-				grille[i][j] = new Cell(i,j,l);
-		}
-	}
+      for (var yoff = -1; yoff <= 1; yoff++) {
+        var j = this.j + yoff;
+        if (j < 0 || j >= colonne) continue;
 
-}
-
-function mousePressed() {
-
-  for (var i = 0; i < ligne; i++) {
-    for (var j = 0; j < colonne; j++) {
-      if (grille[i][j].contenant(mouseX, mouseY)) {
-        grille[i][j].revele();
-
-        if(grille[i][j].bombe){
-          gameOver();
+        var voisine = grille[i][j];
+        if (!voisine.revelee){
+          voisine.revele();
         }
       }
     }
   }
-}
 
-function keyPressed() {
-  if (keyCode = ENTER){
-    for (var i = 0; i < ligne; i++) {
-      for (var j = 0; j < colonne; j++) {
-        if (grille[i][j].contenant(mouseX, mouseY)) {
-          grille[i][j].drap();
-          grille[i][j].revele();
-        }
-      }
-    }
-  }
+ drap(){
+  this.drapeau = true;
+ }
+
+
 }
 
 
-function draw() {
-  background(255);
+function gameOver(){
   for (var i = 0; i < ligne; i++) {
     for (var j = 0; j < colonne; j++) {
-      grille[i][j].show();
+      grille[i][j].revelee = true;
     }
   }
 }
